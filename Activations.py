2b4b62251c6c4bc7980c4e7443b4020f5eff1816
Cloudwalker <pranav13phoenix@gmail.com>
2023-03-14 02:23:10 +0530

    Update Activations.py

--- Activations.py
+++ Activations.py
@@ -5,6 +5,29 @@ import matplotlib.pyplot as plt
 # Load a pretrained model from torchvision
 model = torchvision.models.resnet18(pretrained=False)
 
+#initialize all weights to 1 and bais to 0
+def initialize_weights(m):
+  
+  if isinstance(m, nn.Conv2d):
+      nn.init.constant_(m.weight.data, 1)
+      if m.bias is not None:
+          nn.init.constant_(m.bias.data, 0)
+
+  elif isinstance(m, nn.ConvTranspose2d):
+      nn.init.constant_(m.weight.data, 1)
+      if m.bias is not None:
+          nn.init.constant_(m.bias.data, 0)
+          
+  elif isinstance(m, nn.BatchNorm2d):
+      nn.init.constant_(m.weight.data, 1)
+      nn.init.constant_(m.bias.data, 0)
+
+  elif isinstance(m, nn.Linear):
+      nn.init.constant_(m.weight.data, 1)
+      nn.init.constant_(m.bias.data, 0)
+
+model.apply(initialize_weights)
+
 # Define a forward hook to get the activations for a given layer
 def hook_fn(module, input, output):
     activations = output.detach().cpu().numpy()
